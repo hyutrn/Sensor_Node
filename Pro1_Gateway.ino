280105424772ab9d0279f6dd911cdda480653b6f
TrongHuy <115525478+hyutrn@users.noreply.github.com>
2023-06-22 00:05:57 +0700

    Update Pro1_Gateway.ino

--- Pro1_Gateway.ino
+++ Pro1_Gateway.ino
@@ -1,59 +1,51 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-const char* ssid = "QiQi";
-const char* password = "UyenLe2401";
-
-#define CHANNEL 1 // Kênh ESP-NOW
-typedef struct __attribute__((packed)) {
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message {
   float temperature;
   float humidity;
   float moisture;
-} SensorData;
-SensorData receivedData;
+} struct_message;
 
-void printReceivedData() {
+// Create a struct_message called myData
+struct_message myData;
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Bytes received: ");
   Serial.print("Nhiet do: ");
-  Serial.print(receivedData.temperature);
+  Serial.print(myData.temperature);
   Serial.println(" C");
   Serial.print("Do am khong khi: ");
-  Serial.print(receivedData.humidity);
+  Serial.print(myData.humidity);
   Serial.println(" %");
   Serial.print("Do am dat: ");
-  Serial.print(receivedData.moisture);
+  Serial.print(myData.moisture);
   Serial.println(" %");
   Serial.println();
 }
-
-void onDataReceived(const uint8_t *macAddress, const uint8_t *data, int dataLength) {
-  if (dataLength == sizeof(receivedData)) {
-    memcpy(&receivedData, data, sizeof(receivedData));
-    printReceivedData();
-  }
-}
-
+ 
 void setup() {
+  // Initialize Serial Monitor
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi");
   
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  esp_now_register_recv_cb(onDataReceived);
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
 }
-
+ 
 void loop() {
-  if (Serial.available() > 0) {
-    // Đọc dữ liệu từ Serial để tránh treo chương trình
-    while (Serial.available() > 0) {
-      Serial.read();
-    }
-  }
+
 }
